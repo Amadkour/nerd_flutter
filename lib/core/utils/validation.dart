21cce543612a6f9dbfb4ab8e951253a4b2fbc754
Ahmed <amira.gomaa@res.inc>
2024-05-05 06:33:09 +0300

    login validation

--- lib/core/utils/validation.dart
+++ lib/core/utils/validation.dart
@@ -1,4 +1,26 @@
 class MyValidation {
   static MyValidation instance = MyValidation._init();
   MyValidation._init();
+  final RegExp mailPattern = RegExp(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
+  final RegExp passPattern = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$');
+
+  String? mainValidate(String? input) {
+    ///correct pattern
+    if (mailPattern.hasMatch(input ?? '')) {
+      return null;
+    }
+
+    ///incorrect pattern
+    return "invalid mail";
+  }
+
+  String? passValidate(String? input) {
+    ///correct pattern
+    if (passPattern.hasMatch(input ?? '')) {
+      return null;
+    }
+
+    ///incorrect pattern
+    return "password must be eight characters, at least one letter, one number and one special character";
+  }
 }
